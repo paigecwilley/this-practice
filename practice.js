d002967fc775fdc93d7d871ef769b96bf338da68
Paige <paige.catherine.willey@gmail.com>
2016-02-03 20:41:51 -0700

    First commit

--- practice.js
+++ practice.js
@@ -5,15 +5,19 @@
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
-      //Answer
+      // Implicit 
+      //Explicit
+      New 
+      Default
+
 
   // 3) What is the difference between call and apply?
 
-      //Answer
+      //Call takes in a pointer object and a bunch of items, apply takes in an array
 
   // 4) What does .bind do?
 
-      //Answer
+      //.bind will bind "this" to a newly created function
 
 
 //Next Problem
@@ -23,20 +27,27 @@
   //email --> which is a string
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
-    //Code Here
+    var user = {
+      username:'Paige',
+      email: 'Paige@me.com',
+      getUsername: function() {
+        return this.username;
+
+      }
+    }
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
+user.getUsername();
 
-//Next Problem
 
 
 // Write the function definitions which will make the following function invocations function properly.
 
   //Function Invocations Here
 
-var prius = new Car('Toyota', 'Prius', 2011);
-var mustang = new Car('Ford', 'Mustang', 2013);
+var prius = new Car('Toyota', 'Prius', 2011, 0);
+var mustang = new Car('Ford', 'Mustang', 2013, 0);
 
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
@@ -44,6 +55,17 @@ mustang.moveCar(); //increments mustang' move property by 10. Returns the new mo
 //Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
 
 
+//Create a constructor object
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = move;
+  this.moveCar = function(){
+    return this.move += 10;
+  }
+}
+//Look at the test runner: Test is car hasOwnProperty. put the method on the object inside of the constructor function
 
 //Continuation of previous problem
 
@@ -54,7 +76,8 @@ var getYear = function(){
 //Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
 //Note(no tests)
-  //Code Here
+   getYear.call(prius);
+   getYear.call(mustang); 
 
 
 //New Problem
@@ -75,10 +98,10 @@ setTimeout(getMyUsername, 5000);
 
 //Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
 //Note(no tests)
-  //Answer Here
+  //'iliketurtles'
 
 //In the example above, what is the 'this keyword' bound to when getUsername runs?
 
-  //Answer Here
+  //it's not bound to anything
 
 //Fix the setTimeout invocation so that the user object will be the focal object when getUsername is ran.
